61712bd29f25147a0adb00d72d892a77258a9447
abuXahra <abdulmuminisah@yahoo.com>
2024-12-04 11:42:06 +0100

    updated

--- controller/userController.js
+++ controller/userController.js
@@ -47,3 +47,25 @@ exports.getUser = async (req, res) => {
     console.log(error);
   }
 };
+
+exports.deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
